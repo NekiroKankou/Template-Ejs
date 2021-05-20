cfc085a1cfac261dbd4261486be6ee565ae98bc0
KanakaID <66167985+KanakaID@users.noreply.github.com>
2021-05-20 08:44:42 +0700

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ app.use(express.static(__dirname + '../public'));
 
 const nama = 'Sekolah Harapan Nusa';
 const siswa = [{"nama":"Putri","kelas":"1"},{"nama":"Bambang","kelas":"5"},{"nama":"Hadi","kelas":"2"},{"nama":"Sudan","kelas":"6"}]
-
+siswa = siswa.map(x => `<div class="card"><p>Nama : ${x.name}</p>
+<p>Kelas : ${x.kelas}</p></div>`
 app.get('/',async (req, res) => {
   res.render('index', {siswa:siswa,sekolah:nama})
 })
