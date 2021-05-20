7fdf3bce3138917acf79cf2c0a14c29a8c331c43
KanakaID <66167985+KanakaID@users.noreply.github.com>
2021-05-20 08:38:51 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+var path = require('path');
+const app = express();
+const port = 53653;
+// view engine setup
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+//setup public folder
+app.use(express.static(__dirname + '../public'));
+
+const nama = 'Sekolah Harapan Nusa';
+const siswa = [{"nama":"Putri","kelas":"1"},{"nama":"Bambang","kelas":"5"},{"nama":"Hadi","kelas":"2"},{"nama":"Sudan","kelas":"6"}]
+
+app.get('/',async (req, res) => {
+  res.render('index', {siswa:siswa,sekolah:nama})
+})
