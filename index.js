364dc4684fbf3cc2362547adad7c830ca62a34dd
Kanaka Nakazawa <66167985+KanakaID@users.noreply.github.com>
2021-10-19 21:59:59 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require('express')
 var path = require('path');
 const app = express();
-const port = 53653;
+const port = 8000; //Select Port To Use
+
 // view engine setup
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 //setup public folder
 app.use(express.static(__dirname + '../public'));
 
+//Usage Example
 const nama = 'Sekolah Harapan Nusa';
 const siswa = [{"nama":"Putri","kelas":"1"},{"nama":"Bambang","kelas":"5"},{"nama":"Hadi","kelas":"2"},{"nama":"Sudan","kelas":"6"}]
 siswa = siswa.map(x => `<div class="card"><p>Nama : ${x.name}</p>
